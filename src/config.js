6c2058704c0199613f3e7ff544b7d5461b0d56ac
Juan Miguel Sáez <juanmi_saez@hotmail.com>
2021-05-04 16:59:55 +0200

    add docker compose to run the app

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     APPNAME:'gitHup-api',
-    PORT:'3010',
+    PORT:'3001',
     TOKEN_SECRET: process.env.TOKEN_SECRET || "ce3ff963-39a4-4a9a-ad35-cac22dbdf5a0",
-    DB_CONNECTION:'mongodb://localhost:27017',
+    DB_CONNECTION:process.env.MONGO_NAME || 'mongodb://localhost:27017',
     DB_DATABASE: 'catalog',
     REDIS:{
-        HOST:"127.0.0.1",
+        HOST:process.env.REDIS_NAME || "127.0.0.1",
         PORT:6379
     }
 };
